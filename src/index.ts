7fe24857cb51d61bf9ddadeab6527dc68c4acebd
Viorel Cojocaru <vio@beanon.com>
2025-03-22 12:25:15 +0100

    refactor: Replace relative-ci/agent with relative-ci/core

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import ingest from '@relative-ci/agent/ingest';
-import { filterArtifacts, validateWebpackStats } from '@relative-ci/agent/artifacts';
-import { logResponse, normalizeParams } from '@relative-ci/agent/utils';
+import ingest from '@relative-ci/core/ingest';
+import { filterArtifacts, validateWebpackStats } from '@relative-ci/core/artifacts';
+import loadEnv from '@relative-ci/core/env';
+import { logResponse } from '@relative-ci/core/utils';
 
 import { getWebpackStatsFromFile, getWebpackStatsFromArtifact } from './artifacts';
-import { extractParams, extractPullRequestParams, extractWorkflowRunParams } from './params';
 import { getSummary, logger } from './utils';
 import { AgentParams } from './types';
-import { text } from 'stream/consumers';
 
 const { ACTIONS_STEP_DEBUG, GITHUB_WORKSPACE } = process.env;
 
@@ -32,21 +31,14 @@ async function run() {
       process.env.DEBUG = 'relative-ci:agent';
     }
 
-    // Extract env data
-    let agentParams: AgentParams;
+    // Extract params
+    process.env.RELATIVE_CI_KEY = key;
+    process.env.RELATIVE_CI_SLUG = slug;
+    process.env.RELATIVE_CI_ENDPOINT = endpoint;
 
-    if (eventName === 'pull_request') {
-      logger.debug('Extract params for pull_request flow');
-      agentParams = await extractPullRequestParams(github.context, token, includeCommitMessage);
-    } else if (eventName === 'workflow_run') {
-      logger.debug('Extract params for workflow_run flow');
-      agentParams = await extractWorkflowRunParams(github.context);
-    } else {
-      logger.debug('Extract params for default flow');
-      agentParams = extractParams(github.context);
-    }
+    const params = loadEnv({ agentType: 'github-action' }, { includeCommitMessage });
 
-    logger.debug(`Agent params: ${JSON.stringify(agentParams)}`);
+    logger.debug(`Agent params: ${JSON.stringify(params)}`);
 
     /**
      * Read JSON from the current job or download it from another job's artifact
@@ -67,13 +59,6 @@ async function run() {
     }
 
     validateWebpackStats(webpackStats);
-
-    // Extract params
-    process.env.RELATIVE_CI_KEY = key;
-    process.env.RELATIVE_CI_SLUG = slug;
-    process.env.RELATIVE_CI_ENDPOINT = endpoint;
-
-    const params = normalizeParams(agentParams, { includeCommitMessage });
 
     // Filter artifacts
     const data = filterArtifacts([{ key: 'webpack.stats', data: webpackStats }]);
