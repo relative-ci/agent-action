3d97fdf0d995f7680093f6510a8a8bfeb1a69f42
Viorel Cojocaru <vio@beanon.com>
2025-03-22 12:25:15 +0100

    refactor: Replace relative-ci/agent with relative-ci/core

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,5 @@ import { Context } from '@actions/github/lib/context';
 export type GitHubContext = Context;
 
 export interface AgentParams {
-  commit?: string;
-  branch?: string;
-  pr?: string;
   commitMessage?: string;
 }
