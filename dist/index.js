5b8138b378483b6fc428ff9aba4fbd02caaf3064
Viorel Cojocaru <vio@beanon.com>
2025-05-17 00:40:21 +0200

    chore: Add files

--- dist/index.js
+++ dist/index.js
@@ -166,7 +166,7 @@ async function run() {
     process.env.RELATIVE_CI_KEY = key;
     process.env.RELATIVE_CI_SLUG = slug;
     process.env.RELATIVE_CI_ENDPOINT = endpoint;
-    const params = env_default()({
+    const params = await env_default()({
       agentType: 'github-action'
     }, {
       includeCommitMessage
